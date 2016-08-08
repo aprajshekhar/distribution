b46b95ee2669513f0c54b5afc9cbe0a1f0c31992
A.P. Rajshekhar <randalap@redhat.com>
2016-08-08 17:22:33 +0530

    NA:Changes to service

--- postinstall.sh
+++ postinstall.sh
@@ -1,2 +1,3 @@
 cp /opt/distribution-rri/distribution-rri.service /usr/lib/systemd/system/
 cp /opt/distribution-rri/cmd/registry/config-example.yml /etc/distribution-rri/config.yml
+cp /opt/distribution-rri/bin/registry /usr/bin
